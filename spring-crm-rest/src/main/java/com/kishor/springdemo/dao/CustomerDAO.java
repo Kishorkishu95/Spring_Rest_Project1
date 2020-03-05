34f67c244e465e25a1ad469af20208be3bf5b042
kishor95 <kishorkishu.b@outlook.com>
2020-03-05 16:01:53 +0530

    committed done for spring-crm-rest

---
+++ spring-crm-rest/src/main/java/com/kishor/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.kishor.springdemo.dao;
+
+import java.util.List;
+
+import com.kishor.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
